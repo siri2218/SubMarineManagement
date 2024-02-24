d10be9b271df7c44ceeeec2249e55fcdfc820cda
siri <sireesha.bushan@gmail.com>
2024-02-24 13:57:21 +0530

    Initial changes to commit

---
+++ src/main/java/com/submarine/jacksparrowtask/SubMarineManagementApplication.java
@@ -0,0 +1,13 @@
+package com.submarine.jacksparrowtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubMarineManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubMarineManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/submarine/jacksparrowtask/SubMarineManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.submarine.jacksparrowtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubMarineManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
